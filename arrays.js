3200f70236c6688f653a2b9ec1c224a7807aa220
Alexys Jordan <alexyskjordan@gmail.com>
2022-11-09 19:18:02 -0700

    add a demonstration of understanding

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var friends = ["Madison", "Natalie", "Taylor","Kiki"]
-var ages = [40, 25, 32,10]
+var ages = [40, 25, 32, 10]
 var actualAges = [false, true, true, false]
 
-
+// the index position of "Natalie" is 1
+// the index position of 10 is 3
